523a44663059aff2ba9e8086fb17c84449c199eb
Sathwik <s.mandava@gardin.co.uk>
2022-03-09 13:02:37 +0000

    added comments

--- sm-utils/classifMetrics.py
+++ sm-utils/classifMetrics.py
@@ -10,6 +10,19 @@ def plot_confusion_matrix(
     y_pred_scores: Sequence, 
     labels: Sequence = None
 ):
+    """Creates confusion matrix with confidence scores.
+
+    Parameters:
+    -----------
+    y_true: Sequence
+        true/actual labels
+    y_pred: Sequence
+        predicted labels
+    y_pred_scores: Sequence
+        predicted label confidence score
+    labels: Sequence
+        label names in order to be displayed
+    """
     if not labels:
         labels = np.unique(y_true)
     
@@ -17,8 +30,10 @@ def plot_confusion_matrix(
     y_pred = np.array(y_pred)
     y_pred_scores = np.array(y_pred_scores)
         
-    cm = np.zeros((len(labels), len(labels)))
-    cs = np.zeros((len(labels), len(labels)))
+    cm = np.zeros((len(labels), len(labels))) #Count matrix
+    cs = np.zeros((len(labels), len(labels))) #Confidence score matrix
+
+    #Fill count and mean scores
     for a, l in enumerate(labels):
         for p, ol in enumerate(labels):
             cm[a][p] = (y_pred[y_true==l]==ol).sum()
@@ -28,8 +43,9 @@ def plot_confusion_matrix(
             else:
                 cs[a][p] = scores.mean()
     
-    cn = cm / cm.sum(axis=0, keepdims=True)
+    cn = cm / cm.sum(axis=0, keepdims=True) #Normalised matrix for cell color
     
+    # Annotation labels
     annot_matrix = [
         [
             '{}\n({})'.format(
@@ -45,5 +61,8 @@ def plot_confusion_matrix(
     sns.heatmap(cn, annot=annot_matrix, fmt='', cmap='viridis', ax=axes)
     axes.set_title('Confusion Matrix (with count, confidence)')
     axes.set_xticklabels(labels)
+    axes.set_xlabel("Predicted")
     axes.set_yticklabels(labels)
+    axes.set_ylabel("Actual")
+    axes.xaxis.tick_top()
     fig.show()
